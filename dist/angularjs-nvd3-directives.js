287a73ad974ebc80281d4c3ae28b052e2efc73a2
Robert Kischuk <rob@kischuk.com>
2014-10-18 00:00:11 -0400

    Add barcolor support for multibar chart

--- dist/angularjs-nvd3-directives.js
+++ dist/angularjs-nvd3-directives.js
@@ -1,4 +1,4 @@
-/*! angularjs-nvd3-directives - v0.0.7 - 2014-06-26
+/*! angularjs-nvd3-directives - v0.0.7 - 2014-10-17
  * http://cmaurer.github.io/angularjs-nvd3-directives
  * Copyright (c) 2014 Christian Maurer; Licensed Apache License, v2.0 */
 ( function () {
@@ -128,72 +128,70 @@
         };
       }
     };
-  } ).directive( 'nvd3Legend', [
-    function () {
-      var margin, width, height, id;
-      return {
-        restrict: 'EA',
-        scope: {
-          data: '=',
-          id: '@',
-          margin: '&',
-          width: '@',
-          height: '@',
-          key: '&',
-          color: '&',
-          align: '@',
-          rightalign: '@',
-          updatestate: '@',
-          radiobuttonmode: '@',
-          x: '&',
-          y: '&'
-        },
-        link: function ( scope, element, attrs ) {
-          scope.$watch( 'data', function ( data ) {
-            if ( data ) {
-              if ( scope.chart ) {
-                return d3.select( '#' + attrs.id + ' svg' ).attr( 'height', height ).attr( 'width', width ).datum( data ).transition().duration( 250 ).call( scope.chart );
-              }
-              margin = scope.$eval( attrs.margin ) || {
-                top: 5,
-                right: 0,
-                bottom: 5,
-                left: 0
-              };
-              width = attrs.width === undefined ? element[ 0 ].parentElement.offsetWidth - ( margin.left + margin.right ) : +attrs.width - ( margin.left + margin.right );
-              height = attrs.height === undefined ? element[ 0 ].parentElement.offsetHeight - ( margin.top + margin.bottom ) : +attrs.height - ( margin.top + margin.bottom );
-              if ( width === undefined || width < 0 ) {
-                width = 400;
-              }
-              if ( height === undefined || height < 0 ) {
-                height = 20;
-              }
-              if ( !attrs.id ) {
-                //if an id is not supplied, create a random id.
-                id = 'legend-' + Math.random();
-              } else {
-                id = attrs.id;
-              }
-              nv.addGraph( {
-                generate: function () {
-                  var chart = nv.models.legend().width( width ).height( height ).margin( margin ).align( attrs.align === undefined ? true : attrs.align === 'true' ).rightAlign( attrs.rightalign === undefined ? true : attrs.rightalign === 'true' ).updateState( attrs.updatestate === undefined ? true : attrs.updatestate === 'true' ).radioButtonMode( attrs.radiobuttonmode === undefined ? false : attrs.radiobuttonmode === 'true' ).color( attrs.color === undefined ? nv.utils.defaultColor() : scope.color() ).key( attrs.key === undefined ? function ( d ) {
-                    return d.key;
-                  } : scope.key() );
-                  if ( !d3.select( '#' + attrs.id + ' svg' )[ 0 ][ 0 ] ) {
-                    d3.select( '#' + attrs.id ).append( 'svg' );
-                  }
-                  d3.select( '#' + attrs.id + ' svg' ).attr( 'height', height ).attr( 'width', width ).datum( data ).transition().duration( 250 ).call( chart );
-                  nv.utils.windowResize( chart.update );
-                  scope.chart = chart;
-                  return chart;
-                }
-              } );
+  } ).directive( 'nvd3Legend', [ function () {
+    var margin, width, height, id;
+    return {
+      restrict: 'EA',
+      scope: {
+        data: '=',
+        id: '@',
+        margin: '&',
+        width: '@',
+        height: '@',
+        key: '&',
+        color: '&',
+        align: '@',
+        rightalign: '@',
+        updatestate: '@',
+        radiobuttonmode: '@',
+        x: '&',
+        y: '&'
+      },
+      link: function ( scope, element, attrs ) {
+        scope.$watch( 'data', function ( data ) {
+          if ( data ) {
+            if ( scope.chart ) {
+              return d3.select( '#' + attrs.id + ' svg' ).attr( 'height', height ).attr( 'width', width ).datum( data ).transition().duration( 250 ).call( scope.chart );
             }
-          } );
-        }
-      };
-    }
-  ] );
+            margin = scope.$eval( attrs.margin ) || {
+              top: 5,
+              right: 0,
+              bottom: 5,
+              left: 0
+            };
+            width = attrs.width === undefined ? element[ 0 ].parentElement.offsetWidth - ( margin.left + margin.right ) : +attrs.width - ( margin.left + margin.right );
+            height = attrs.height === undefined ? element[ 0 ].parentElement.offsetHeight - ( margin.top + margin.bottom ) : +attrs.height - ( margin.top + margin.bottom );
+            if ( width === undefined || width < 0 ) {
+              width = 400;
+            }
+            if ( height === undefined || height < 0 ) {
+              height = 20;
+            }
+            if ( !attrs.id ) {
+              //if an id is not supplied, create a random id.
+              id = 'legend-' + Math.random();
+            } else {
+              id = attrs.id;
+            }
+            nv.addGraph( {
+              generate: function () {
+                var chart = nv.models.legend().width( width ).height( height ).margin( margin ).align( attrs.align === undefined ? true : attrs.align === 'true' ).rightAlign( attrs.rightalign === undefined ? true : attrs.rightalign === 'true' ).updateState( attrs.updatestate === undefined ? true : attrs.updatestate === 'true' ).radioButtonMode( attrs.radiobuttonmode === undefined ? false : attrs.radiobuttonmode === 'true' ).color( attrs.color === undefined ? nv.utils.defaultColor() : scope.color() ).key( attrs.key === undefined ? function ( d ) {
+                  return d.key;
+                } : scope.key() );
+                if ( !d3.select( '#' + attrs.id + ' svg' )[ 0 ][ 0 ] ) {
+                  d3.select( '#' + attrs.id ).append( 'svg' );
+                }
+                d3.select( '#' + attrs.id + ' svg' ).attr( 'height', height ).attr( 'width', width ).datum( data ).transition().duration( 250 ).call( chart );
+                nv.utils.windowResize( chart.update );
+                scope.chart = chart;
+                return chart;
+              }
+            } );
+          }
+        } );
+      }
+    };
+  } ] );
 
   function initializeLegendMargin( scope, attrs ) {
     var margin = ( scope.$eval( attrs.legendmargin ) || {
@@ -1278,6 +1276,7 @@
           tooltips: '@',
           tooltipcontent: '&',
           color: '&',
+          barcolor: '&',
           showcontrols: '@',
           nodata: '@',
           reducexticks: '@',
@@ -1375,6 +1374,9 @@
                   } : scope.y() ).forceY( attrs.forcey === undefined ? [ 0 ] : scope.$eval( attrs.forcey ) ).showLegend( attrs.showlegend === undefined ? false : attrs.showlegend === 'true' ).showControls( attrs.showcontrols === undefined ? false : attrs.showcontrols === 'true' ).showXAxis( attrs.showxaxis === undefined ? false : attrs.showxaxis === 'true' ).showYAxis( attrs.showyaxis === undefined ? false : attrs.showyaxis === 'true' ).tooltips( attrs.tooltips === undefined ? false : attrs.tooltips === 'true' ).reduceXTicks( attrs.reducexticks === undefined ? false : attrs.reducexticks === 'true' ).staggerLabels( attrs.staggerlabels === undefined ? false : attrs.staggerlabels === 'true' ).noData( attrs.nodata === undefined ? 'No Data Available.' : scope.nodata ).rotateLabels( attrs.rotatelabels === undefined ? 0 : attrs.rotatelabels ).color( attrs.color === undefined ? nv.utils.defaultColor() : scope.color() ).delay( attrs.delay === undefined ? 1200 : attrs.delay ).stacked( attrs.stacked === undefined ? false : attrs.stacked === 'true' );
                   if ( attrs.tooltipcontent ) {
                     chart.tooltipContent( scope.tooltipcontent() );
+                  }
+                  if ( attrs.barcolor ) {
+                    chart.bars.barColor( scope.barcolor() );
                   }
                   scope.d3Call( data, chart );
                   nv.utils.windowResize( chart.update );
